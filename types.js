09918f4dc46a914f26707058fc961ec2749f5d22
abushahed670 <abu.shahed670@gmail.com>
2024-11-15 00:08:19 +0600

    math variable types aded

---
+++ types.js
@@ -0,0 +1,8 @@
+var name = "sada uddin";
+var address  = "andor killa bandornban";
+var country = 'bangladesh';
+
+var isRich = true;
+
+console.log(isRich);
+console.log(typeof isRich);
---
+++ variable.js
@@ -0,0 +1,18 @@
+// 5 things to declare a variable
+
+var weight = 38;
+var price = 35;
+var year = 2020;
+
+
+console.log(weight);
+console.log(year);
+
+
+
+
+
+// new 
+var age = 17;
+age=35;
+console.log(age)
